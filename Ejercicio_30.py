9c72ccd221f678ae35a842ec77e812d6cbbb8000
Jose Adolfo Arias <116679678+Jadoarias@users.noreply.github.com>
2024-07-29 22:08:48 -0500

    Add files via upload

---
+++ Ejercicio_30.py
@@ -0,0 +1,8 @@
+def caracter():
+    carac = input("Ingrese un caracter: ")
+    if carac == "a" or carac == "e" or carac == "i" or carac == "o" or carac == "u":
+        print("True")
+    else:
+        print("False")
+
+caracter()
