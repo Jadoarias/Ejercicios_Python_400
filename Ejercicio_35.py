3abb295dc5e19db0a22e4871df23abeaaaca0556
Jose Adolfo Arias <116679678+Jadoarias@users.noreply.github.com>
2024-07-30 16:10:36 -0500

    Add files via upload

---
+++ Ejercicio_35.py
@@ -0,0 +1,6 @@
+def generar_n_caracteres():
+    caracter = input("Ingresa el caracter: ")
+    entero = int(input("Ingresa un numero entero: "))
+    print(entero*caracter)
+
+generar_n_caracteres()
