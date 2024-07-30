61ca3fbeceae72c7573fd91825138d93287fda38
Jose Adolfo Arias <116679678+Jadoarias@users.noreply.github.com>
2024-07-29 21:49:47 -0500

    Add files via upload

---
+++ Ejercicio_29.py
@@ -0,0 +1,12 @@
+def longitudtexto(a, b, c, d):
+    lista = [a,b,c,d]
+    print(lista)
+
+    contador = 0
+    for i in lista:
+        contador = contador+1
+    print(contador)
+
+
+longitudtexto(1,3,4,5)
+    
