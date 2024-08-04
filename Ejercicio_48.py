b155c54178ad7725f525792f45b7fb99ed16f4ea
Jose Adolfo Arias <116679678+Jadoarias@users.noreply.github.com>
2024-08-03 22:24:52 -0500

    Delete Ejercicio_48.py

--- Ejercicio_48.py
+++
@@ -1,8 +0,0 @@
-lista = ['perro', 'gato', 'gallina', 'perro', 'vaca', 'vaca', 'delfin']
-
-def EliminarDuplicados():
-    print(lista)
-    lista_conjuntos = set(lista)
-    print(lista_conjuntos)
-
-EliminarDuplicados()
