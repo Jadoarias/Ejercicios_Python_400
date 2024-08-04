ce9db0adfedff309e46432734fcf9ce6a6b0ee2c
Jose Adolfo Arias <116679678+Jadoarias@users.noreply.github.com>
2024-08-03 22:24:22 -0500

    Add files via upload

---
+++ Ejercicio_48.py
@@ -0,0 +1,8 @@
+lista = ['perro', 'gato', 'gallina', 'perro', 'vaca', 'vaca', 'delfin']
+
+def EliminarDuplicados():
+    print(lista)
+    lista_conjuntos = set(lista)
+    print(lista_conjuntos)
+
+EliminarDuplicados()
